a9437f1836d5102ebc8fd5d410d944e76b289e7c
aaaaakihiro <gatwmpa26759@gmail.com>
2020-04-26 17:47:21 +0900

    lesson2

---
+++ main.js
@@ -0,0 +1,19 @@
+const breakfast="トースト"
+　
+　//console.log("今日の朝は" +breakfast +"を食べました。")
+
+//console.log('今日の朝は${breakfast}を食べました。')
+
+//const days=['日','月','火','水','木','金','土']
+
+//console.log(days.length)
+
+const nengo=['明治','大正','昭和','平成']
+
+nengo[1]='昭和'
+
+console.log(nengo[1])
+const point=100
+
+if(point>80){alert('合格')}
+else{alert('不合格です')}
